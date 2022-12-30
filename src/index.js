c9ba5b15969321aa07a4f43f4d55472f69a65314
aswinhappyclose <aswin@happyclose.net>
2022-12-30 11:25:24 +0530

    "code"

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './components/screens/home/Home';
-import Close from './components/screens/product/Product.js';
-import Blog from './components/screens/blog/Blog';
-import AboutUs from './components/screens/aboutUs/AboutUs';
-import ScheduleDemo from './components/screens/scheduleDemo/ScheduleDemo';
-import { ToastContainer} from 'react-toastify';
-import "./components/Global.css"
+import Home from "./components/screens/home/Home";
+import Close from "./components/screens/product/Product.js";
+import Blog from "./components/screens/blog/Blog";
+import AboutUs from "./components/screens/aboutUs/AboutUs";
+import ScheduleDemo from "./components/screens/scheduleDemo/ScheduleDemo";
+import { ToastContainer } from "react-toastify";
+import "./components/Global.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ToastContainer />
+      <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/product" element={<Close/>}/>
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/about" element={<AboutUs/>}/>
-        <Route path="/scheduledemo" element={<ScheduleDemo/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Close />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/scheduledemo" element={<ScheduleDemo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
